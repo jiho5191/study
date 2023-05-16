732c16ff00ae6c8d55ef9ab1026c8500d98857cb
jiho5191 <133729269+jiho5191@users.noreply.github.com>
2023-05-16 12:59:41 +0900

    Add files via upload

---
+++ 자료구조 7주차 과제.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+typedef int element;
+typedef struct ListNode {	// 노드 타입
+	element data;
+	struct ListNode* link;
+} ListNode;
+
+//원형 연결 리스트의 처음에 삽입
+ListNode* insert_first(ListNode* head, element data)
+{
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+	if (head == NULL) {
+		head = node;
+		node->link = head;
+	}
+	else {
+		node->link = head->link;	//(1)
+		head->link = node;			//(2)
+	}
+	return head;	//변경된 헤드 포인터를 반환한다.
+}
+
+//원형 연결 리스트의 끝에 삽입
+ListNode* insert_last(ListNode* head, element data)
+{
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+	if (head == NULL) {
+		head = node;
+		node->link = head;
+	}
+	else {
+		node->link = head->link;	//(1)
+		head->link = node;			//(2)
+		head = node;				//(3)
+	}
+	return head;	//변경된 헤드 포인터를 반환한다.
+}
+
+// 리스트의 항목 출력
+void print_list(ListNode* head)
+{
+	ListNode* p;
+
+	if (head == NULL) return;
+	p = head->link;
+	do {
+		printf("%d->", p->data);
+		p = p->link;
+	} while (p != head);
+	printf("%d->", p->data); // 마지막 노드 출력
+}
+
+int main(void)
+{
+	ListNode* head = NULL;
+
+	// list = 10->20->30->40
+	head = insert_last(head, 20);
+	head = insert_last(head, 30);
+	head = insert_last(head, 40);
+	head = insert_first(head, 10);
+	print_list(head);
+
+	return 0;
+}
+*/
+
+typedef int element;
+typedef struct DListNode {		// 이중연결 노드 타입
+	element data;
+	struct DListNode* llink;
+	struct DListNode* rlink;
+} DListNode;
+
+// 이중 연결 리스트를 초기화
+void init(DListNode* phead)
+{
+	phead->llink = phead;
+	phead->rlink = phead;
+}
+
+// 이중 연결 리스트의 노드를 출력
+void print_dlist(DListNode* phead)
+{
+	DListNode* p;
+	for (p = phead->rlink; p != phead; p = p->rlink) {
+		printf("<-| |%d| |->", p->data);
+	}
+	printf("\n");
+}
+
+// 새로운 데이터를 노드 before의 오른쪽에 삽입한다.
+void dinsert(DListNode* before, element data)
+{
+	DListNode* newnode = (DListNode*)malloc(sizeof(DListNode));
+	strcpy(newnode->data, data);
+	newnode->llink = before;
+	newnode->rlink = before->rlink;
+	before->rlink->llink = newnode;
+	before->rlink = newnode;
+}
+
+// 노드 removed를 삭제한다.
+void ddelete(DListNode* head, DListNode* removed)
+{
+	if (removed == head) return;
+	removed->llink->rlink = removed->rlink;
+	removed->rlink->llink = removed->llink;
+	free(removed);
+}
+
+// 이중 연결 리스트 테스트 프로그램
+int main(void)
+{
+	DListNode* head = (DListNode*)malloc(sizeof(DListNode));
+	init(head);
+	printf("추가 단계\n");
+	for (int i = 0; i < 5; i++) {
+		//헤드 노드의 오른쪽에 삽입
+		dinsert(head, i);
+		print_dlist(head);
+	}
+	printf("\n삭제 단계\n");
+	for (int i = 0; i < 5; i++) {
+		print_dlist(head);
+		ddelete(head, head->rlink);
+	}
+	free(head);
+	return 0;
+}
