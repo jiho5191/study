040c96df1f0efbb3ac80346aa81bae62e6fd945c
jiho5191 <133729269+jiho5191@users.noreply.github.com>
2023-08-02 16:52:36 +0900

    Update 연습문제.py

--- 연습문제.py
+++ 연습문제.py
@@ -1,4 +1,4 @@
-"""
+
 # 연습문제 1
 iot=int(input("IoT 점수 : "))
 ai=int(input("인공지능 점수 : "))
@@ -32,7 +32,7 @@ totalP=(icecream*icecreamP)+(noodle*noodleP)+(snack*snackP)+(bread*breadP)
 print("총 금액 : " + str(totalP))
 print("총 금액 : ", totalP)
 print("총 금액 %d원 입니다.\n" % totalP)
-"""
+
 
 # 연습문제 3
 from math import pi
@@ -44,10 +44,19 @@ print("라디안 : %f\n" % Radian)
 
 # 연습문제 4
 print("원기둥의 표면 부피와 면적을 구하는 프로그램\n")
-pi=3.14
-H=float(input("높이 : "))
-R=float(input("반지름 : "))
-V=pi*H*R*R
-A=((2*pi*R)*H)+((pi*R*R*2))
-print("부피 : %.2f\n면적 : %.2f\n" % (V,A))
 
+# 변수 생성
+pi=3.14
+h=0.0   # 높이
+r=0     # 반지름
+
+V=0.0   # 표면 부피
+A=0.0   # 표면 면적
+
+h=float(input("높이 입력(실수) : "))
+r=int(input("반지름 입력(정수) : "))
+
+V = pi * h * (r ** 2)
+A = (( 2 * pi * r ) * h ) + (( pi * ( r **2 ) *2))
+
+print("부피 : %.2f\n면적 : %.2f\n" % (V,A))
