830fe838a22d872359b985f60cd57e9c65e66577
jiho5191 <133729269+jiho5191@users.noreply.github.com>
2023-08-07 14:20:36 +0900

    Update 230805_반복문_실습3.py

--- 230805_반복문_실습3.py
+++ 230805_반복문_실습3.py
@@ -1,15 +1,22 @@
 # 평균을 구하고 합격, 불합격을 출력하는 프로그램
 
+subject=0   # 과목 수 입력
+sum=0.0     # 총점
+score=0.0   # 점수 입력
+aver=0.0    # 평균
+
 subject=int(input("입력할 과목 개수(10과목 이하) : "))
-sum=0
 
-for i in range(subject) :
-    score=int(input("%d. 과목 점수 : " % (i+1)))
-    sum+=score
+if ( subject > 0 and subject < 11 ) :
+    for i in range(subject):
+        score = float(input("%d. 과목 점수 : " % (i + 1)))
+        sum += score
 
-aver=sum/subject
+    aver = sum / subject
 
-if ( aver > 80 ) :
-    print("평균 : %.1f    합격" % aver)
+    if (aver > 80):
+        print("평균 : %.1f    합격" % aver)
+    else:
+        print("평균 : %.1f    불합격" % aver)
 else :
-    print("평균 : %.1f    불합격" % aver)
+    print("과목 수는 0보다 크고 10이하의 수만 가능합니다.")
